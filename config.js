3e6f2f031c81038136c4c740b6bc8e8c7a9f5dec
jcmacallan-svg <j.c.macallan@gmail.com>
2026-02-24 23:06:06 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js (optional)
-window.CONFIG = window.CONFIG || {
-  assetBase: "assets/photos",
-  headshotPrefix: "headshot_",
-  headshotCount: 10,
-  voiceAutosend: true,
-  debug: true
-};
