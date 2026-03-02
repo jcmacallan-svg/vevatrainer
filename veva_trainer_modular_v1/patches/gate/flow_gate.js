7f3dd3215ac145bf96e4347c2770fb8254884b31
jcmacallan-svg <j.c.macallan@gmail.com>
2026-03-02 18:24:02 +0100

    Update flow_gate.js

--- veva_trainer_modular_v1/patches/gate/flow_gate.js
+++ veva_trainer_modular_v1/patches/gate/flow_gate.js
@@ -1,2 +1,8 @@
 // patches/gate/flow_gate.js
-(function(){ window.VEVA_FLOWS = window.VEVA_FLOWS || {}; window.VEVA_FLOWS.gate = { name:"Gate", firstStage:"gate_approach" }; })();
+(function(){
+  window.VEVA_FLOWS = window.VEVA_FLOWS || {};
+  window.VEVA_FLOWS.gate = {
+    name: "Gate",
+    firstStage: "gate_approach"
+  };
+})();
