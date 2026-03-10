7cb41b1d7143024f602c415332a34a2c900afb45
jcmacallan-svg <j.c.macallan@gmail.com>
2026-03-10 16:32:05 +0100

    Delete veva_trainer_modular_v1 directory

--- veva_trainer_modular_v1/patches/person_search/flow_person_search.js
+++
@@ -1,2 +0,0 @@
-// patches/person_search/flow_person_search.js
-(function(){ window.VEVA_FLOWS = window.VEVA_FLOWS || {}; window.VEVA_FLOWS.person_search = { name:"Person Search", firstStage:"ps_arrival" }; })();
